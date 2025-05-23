5686e9bf5ae2484f2a638c11385564e262e6915f
leducthang <leducthang72005@gmail.com>
2025-05-23 21:40:29 +0700

    first change

--- api.py
+++
@@ -1,8 +0,0 @@
-import elgamal
-from flask import Flask, request,  jsonify
-app = Flask(__name__)
-@app.route("/encrypted", methods=["POST"])
-def encryted():
-    data = request.get_json()
-    message = data.get("message")
-    encryted = elgamal.ma_hoa()
